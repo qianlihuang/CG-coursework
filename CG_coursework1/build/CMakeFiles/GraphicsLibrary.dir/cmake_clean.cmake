c7ce1a1bb29d48630c6c4df1406da617f64850e8
q <1098822169@qq.com>
2024-12-23 16:25:04 +0800

    Add generated CMake files for build system setup

---
+++ CG_coursework1/build/CMakeFiles/GraphicsLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphicsLibrary.dir/src/graphics.cpp.obj"
+  "CMakeFiles/GraphicsLibrary.dir/src/graphics.cpp.obj.d"
+  "CMakeFiles/GraphicsLibrary.dir/src/main.cpp.obj"
+  "CMakeFiles/GraphicsLibrary.dir/src/main.cpp.obj.d"
+  "GraphicsLibrary.exe"
+  "GraphicsLibrary.exe.manifest"
+  "GraphicsLibrary.pdb"
+  "libGraphicsLibrary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphicsLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CG_coursework1/build/CMakeFiles/GraphicsLibrary.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for GraphicsLibrary.
+# This may be replaced when dependencies are built.
